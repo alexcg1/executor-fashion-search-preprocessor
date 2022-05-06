d724e4a956e05a64ddb79fbc952b9c1b5e68446f
Alex C-G <alexcg@outlook.com>
2022-05-06 16:45:09 +0200

    feat: expose all params, clean up, fix image to image

--- executor.py
+++ executor.py
@@ -1,52 +1,6 @@
 from jina import Executor, DocumentArray, requests
+import numpy as np
 import random
-import os
-
-
-def generate_price(price_range=(10, 200)):
-    price = random.randrange(price_range[0], price_range[1])
-
-    return price
-
-def generate_uri(doc, data_dir=".", file_ext="jpg"):
-    doc.uri = f"{data_dir}/{doc.id}.{file_ext}"
-
-    return doc
-
-
-def preproc(doc, tensor_shape=(80, 60)):
-    if not doc.text:
-        # ensure we have a tensor
-        if doc.uri:
-            doc.load_uri_to_image_tensor()
-        elif doc.blob:
-            doc.convert_blob_to_image_tensor()
-
-        # Apply settings to tensor
-        doc.set_image_tensor_shape(tensor_shape).set_image_tensor_normalization()
-
-    return doc
-
-
-def add_metadata(doc, data_dir="./data", rating_range=(0, 5), price_range=(10, 200)):
-    # Fix uri
-    if not data_dir:
-        data_dir = "."
-
-    if hasattr(doc, "id"):
-        filename = f"{data_dir}/{doc.id}.jpg"
-        doc.uri = filename
-
-    # Generate fake price
-    if price_range:
-        doc.tags["price"] = generate_price(price_range)
-
-    # Generate fake rating based on id
-    if rating_range:
-        random.seed(int(doc.id))  # Ensure reproducability
-        doc.tags["rating"] = random.randrange(rating_range[0], rating_range[1])
-
-    return doc
 
 
 class FashionSearchPreprocessor(Executor):
@@ -56,22 +10,73 @@ class FashionSearchPreprocessor(Executor):
         tensor_shape=(80, 60),
         rating_range=(0, 5),
         price_range=(10, 200),
+        file_ext="jpg",
+        *args,
         **kwargs,
     ):
-        super().__init__(**kwargs)
+        super().__init__(*args, **kwargs)
         self.data_dir = data_dir
         self.tensor_shape = tensor_shape
         self.price_range = price_range
         self.rating_range = rating_range
+        self.file_ext = file_ext
 
     @requests(on="/index")
     def process_index_document(self, docs: DocumentArray, **kwargs):
         for doc in docs:
-            doc = generate_uri(doc, data_dir=self.data_dir)
-            doc = preproc(doc)
-            doc = add_metadata(doc, data_dir=self.data_dir)
+            doc = self._generate_uri(doc)
+            doc = self._preproc(doc)
+            doc = self._add_metadata(doc)
 
     @requests(on="/search")
     def process_search_document(self, docs: DocumentArray, **kwargs):
         for doc in docs:
-            doc = preproc(doc, "../data/images")
+            doc = self._preproc(doc)
+            print(doc)
+
+    def _generate_price(self):
+        price = random.randrange(self.price_range[0], self.price_range[1])
+
+        return price
+
+    def _generate_uri(self, doc):
+        doc.uri = f"{self.data_dir}/{doc.id}.{self.file_ext}"
+
+        return doc
+
+    def _preproc(self, doc):
+        if not doc.text:
+            # ensure we have a tensor
+            if doc.uri:
+                doc.load_uri_to_image_tensor()
+            elif doc.blob:
+                doc.convert_blob_to_image_tensor()
+
+            # Apply settings to tensor
+            doc.tensor = doc.tensor.astype(np.uint8)
+            doc.set_image_tensor_shape(shape=self.tensor_shape)
+            doc.set_image_tensor_normalization()
+
+        return doc
+
+    def _add_metadata(self, doc):
+        # Fix uri
+        # if not self.data_dir:
+        # self.data_dir = "."
+
+        # if hasattr(doc, "id"):
+        # filename = f"{self.data_dir}/{doc.id}.jpg"
+        # doc.uri = filename
+
+        # Generate fake price
+        if self.price_range:
+            doc.tags["price"] = self._generate_price()
+
+        # Generate fake rating based on id
+        if self.rating_range:
+            random.seed(int(doc.id))  # Ensure reproducability
+            doc.tags["rating"] = random.randrange(
+                self.rating_range[0], self.rating_range[1]
+            )
+
+        return doc
